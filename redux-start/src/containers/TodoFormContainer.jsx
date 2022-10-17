0128fb850b9c9d57508faeb69486a56fdd1fdb5c
yoongmi <78575434+yoongmi@users.noreply.github.com>
2022-10-17 21:37:00 +0900

    hook을 이용한 연결

--- redux-start/src/containers/TodoFormContainer.jsx
+++ redux-start/src/containers/TodoFormContainer.jsx
@@ -1,14 +1,18 @@
-import { connect } from "react-redux";
+import { useCallback } from "react";
+import { useDispatch } from "react-redux";
 import TodoForm from "../components/TodoForm";
 import { addTodo } from "../redux/actions";
 
-const ToDoFormContainer = connect(
-  (state) => ({}),
-  (dispatch) => ({
-    add: (text) => {
+function ToDoFormContainer() {
+  const dispatch = useDispatch();
+  const add = useCallback(
+    (text) => {
       dispatch(addTodo(text));
     },
-  })
-)(TodoForm);
+    [dispatch]
+  );
+
+  return <TodoForm add={add} />;
+}
 
 export default ToDoFormContainer;
--- redux-start/src/containers/TodoListContainer.jsx
+++ redux-start/src/containers/TodoListContainer.jsx
@@ -1,18 +1,9 @@
-import { connect } from "react-redux";
+import { useSelector } from "react-redux";
 import TodoList from "../components/TodoList";
 
-const mapStateToProps = (state) => {
-  return {
-    todos: state.todos,
-  };
-};
-const mapDispatchToProps = (dispatch) => {
-  return {};
-};
-
-const TodoListContainer = connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(TodoList);
+function TodoListContainer() {
+  const todos = useSelector((state) => state.todos);
+  return <TodoList todos={todos} />;
+}
 
 export default TodoListContainer;
