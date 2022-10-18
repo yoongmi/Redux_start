d760a1f05b5cdf839a40456bc7102227faa26a83
yoongmi <78575434+yoongmi@users.noreply.github.com>
2022-10-18 17:41:32 +0900

    리덕스 미들웨어

--- redux-start/src/redux/store.js
+++ redux-start/src/redux/store.js
@@ -1,6 +1,32 @@
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
 import todoApp from "./reducers/reducer";
 
-const store = createStore(todoApp);
+function middleware1(store) {
+  console.log("middleware1", 0);
+  return (next) => {
+    console.log("middleware1", 1, next);
+    return (action) => {
+      console.log("middleware1", 2);
+      const returnValue = next(action);
+      console.log("middleware1", 3);
+      return returnValue;
+    };
+  };
+}
+
+function middleware2(store) {
+  console.log("middleware2", 0);
+  return (next) => {
+    console.log("middleware2", 1, next);
+    return (action) => {
+      console.log("middleware2", 2);
+      const returnValue = next(action);
+      console.log("middleware2", 3);
+      return returnValue;
+    };
+  };
+}
+
+const store = createStore(todoApp, applyMiddleware(middleware1, middleware2));
 
 export default store;
