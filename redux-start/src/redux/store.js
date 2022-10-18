fab6c405c05d4cd4cf9452d8ded89a690ec72336
yoongmi <78575434+yoongmi@users.noreply.github.com>
2022-10-18 17:47:42 +0900

    redux-devtools

--- redux-start/src/redux/store.js
+++ redux-start/src/redux/store.js
@@ -1,32 +1,33 @@
 import { applyMiddleware, createStore } from "redux";
 import todoApp from "./reducers/reducer";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-function middleware1(store) {
-  console.log("middleware1", 0);
-  return (next) => {
-    console.log("middleware1", 1, next);
-    return (action) => {
-      console.log("middleware1", 2);
-      const returnValue = next(action);
-      console.log("middleware1", 3);
-      return returnValue;
-    };
-  };
-}
+// function middleware1(store) {
+//   console.log("middleware1", 0);
+//   return (next) => {
+//     console.log("middleware1", 1, next);
+//     return (action) => {
+//       console.log("middleware1", 2);
+//       const returnValue = next(action);
+//       console.log("middleware1", 3);
+//       return returnValue;
+//     };
+//   };
+// }
 
-function middleware2(store) {
-  console.log("middleware2", 0);
-  return (next) => {
-    console.log("middleware2", 1, next);
-    return (action) => {
-      console.log("middleware2", 2);
-      const returnValue = next(action);
-      console.log("middleware2", 3);
-      return returnValue;
-    };
-  };
-}
+// function middleware2(store) {
+//   console.log("middleware2", 0);
+//   return (next) => {
+//     console.log("middleware2", 1, next);
+//     return (action) => {
+//       console.log("middleware2", 2);
+//       const returnValue = next(action);
+//       console.log("middleware2", 3);
+//       return returnValue;
+//     };
+//   };
+// }
 
-const store = createStore(todoApp, applyMiddleware(middleware1, middleware2));
+const store = createStore(todoApp, composeWithDevTools(applyMiddleware()));
 
 export default store;
